bd3394ea1533060844273c53623782c1e411baa7
Mahaub Alam <mahabubalom72091@gmail.com>
2025-05-10 08:51:01 +0300

    update have some error

--- server/index.js
+++ server/index.js
@@ -130,6 +130,23 @@ async function run() {
     });
 
     // meune reletd api
+    app.patch("/menu/:id", async (req, res) => {
+      const item = req.body;
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          name: item.name,
+          category: item.category,
+          price: item.price,
+          recipe: item.recipe,
+          image: item.image,
+        },
+      };
+
+      const result = await menuCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
 
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
