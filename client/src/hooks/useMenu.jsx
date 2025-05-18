2742ead2170cb9df2a4d7f0196f2bc61bac68f5a
Mahaub Alam <mahabubalom72091@gmail.com>
2025-05-18 17:57:10 +0300

    added

--- client/src/hooks/useMenu.jsx
+++ client/src/hooks/useMenu.jsx
@@ -5,24 +5,7 @@ import useAxiosPublic from "./useAxiosPublic";
 
 const useMenu = () => {
   const axiosPublic = useAxiosPublic();
-  // const [menu, setMenu] = useState([]);
-  // const [loading, setLoading] = useState(true);
-  // useEffect(() => {
-  //     fetch('http://localhost:5000/menu')
-  //         .then(res => res.json())
-  //         .then(data => {
-  //             setMenu(data);
-  //             setLoading(false);
-  //         });
-  // }, [])
-  // useEffect(() => {
-  //     fetch('http://localhost:5000/menu')
-  //         .then(res => res.json())
-  //         .then(data => {
-  //             setMenu(data);
-  //             setLoading(false);
-  //         });
-  // }, [])
+
 
   const {
     data: menu = [],
