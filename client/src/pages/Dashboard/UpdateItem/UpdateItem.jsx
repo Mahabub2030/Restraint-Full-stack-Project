da09766172e66c0a158337909547007ff0c762b5
Mahaub Alam <mahabubalom72091@gmail.com>
2025-06-10 12:36:57 +0300

    some code update

--- client/src/pages/Dashboard/UpdateItem/UpdateItem.jsx
+++ client/src/pages/Dashboard/UpdateItem/UpdateItem.jsx
@@ -1,64 +1,62 @@
 
 import { useLoaderData } from "react-router-dom";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
-// import { useForm } from "react-hook-form";
-// import Swal from "sweetalert2";
-// import useAxiosPublic from "../../../hooks/useAxiosPublic";
-// import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
-// const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
-// const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
+const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 
 const UpdateItem = () => {
-    // const { name, category, recipe, price, _id } = useLoaderData();
-    // console.log(name, category, recipe, price, _id);
+    const { name, category, recipe, price, _id } = useLoaderData();
+    console.log(name, category, recipe, price, _id);
     const loaderData = useLoaderData();
 console.log(loaderData); // Check if loaderData is correct
 
 // const { name, category, recipe, price, _id } = useLoaderData || {};
 
 
-
-   
-//   const { register, handleSubmit } = useForm();
-//   const axiosPublic = useAxiosPublic();
-//   const axiosSecure = useAxiosSecure();
-//   const onSubmit = async (data) => {
-//     console.log(data);
-//     console.log(name);
-//     // image upload to imgbb and then get an url
-//     const imageFile = { image: data.image[0] };
-//     const res = await axiosPublic.post(image_hosting_api, imageFile, {
-//       headers: {
-//         "content-type": "multipart/form-data",
-//       },
-//     });
-//     if (res.data.success) {
-//       // now send the menu item data to the server with the image url
-//       const menuItem = {
-//         name: data.name,
-//         category: data.category,
-//         price: parseFloat(data.price),
-//         recipe: data.recipe,
-//         image: res.data.data.display_url,
-//       };
-//       //
-//       const menuRes = await axiosSecure.patch(`/menu/${_id}`, menuItem);
-//       console.log(menuRes.data);
-//       if (menuRes.data.modifiedCount > 0) {
-//         // show success popup
-//         // reset();
-//         Swal.fire({
-//           position: "top-end",
-//           icon: "success",
-//           title: `${data.name} is updated to the menu.`,
-//           showConfirmButton: false,
-//           timer: 1500,
-//         });
-//       }
-//     }
-//     console.log("with image url", res.data);
-//   };
+  const { register, handleSubmit } = useForm();
+  const axiosPublic = useAxiosPublic();
+  const axiosSecure = useAxiosSecure();
+  const onSubmit = async (data) => {
+    console.log(data);
+    console.log(name);
+    // image upload to imgbb and then get an url
+    const imageFile = { image: data.image[0] };
+    const res = await axiosPublic.post(image_hosting_api, imageFile, {
+      headers: {
+        "content-type": "multipart/form-data",
+      },
+    });
+    if (res.data.success) {
+      // now send the menu item data to the server with the image url
+      const menuItem = {
+        name: data.name,
+        category: data.category,
+        price: parseFloat(data.price),
+        recipe: data.recipe,
+        image: res.data.data.display_url,
+      };
+      //
+      const menuRes = await axiosSecure.patch(`/menu/${_id}`, menuItem);
+      console.log(menuRes.data);
+      if (menuRes.data.modifiedCount > 0) {
+        // show success popup
+        reset();
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: `${data.name} is updated to the menu.`,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    }
+    console.log("with image url", res.data);
+  };
 
   return (
     <div>
@@ -67,8 +65,7 @@ console.log(loaderData); // Check if loaderData is correct
         subHeading="Refresh info"
       ></SectionTitle>
       <div>
-        <form >
-        {/* <form onSubmit={handleSubmit(onSubmit)}> */}
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="form-control w-full my-6">
             <label className="label">
               <span className="label-text">Recipe Name*</span>
@@ -78,7 +75,7 @@ console.log(loaderData); // Check if loaderData is correct
               defaultValue={name}
               placeholder="Recipe Name"
           
-            //   {...register("name", { required: true })}
+              {...register("name", { required: true })}
               required
               className="input input-bordered w-full"
             />
@@ -91,9 +88,9 @@ console.log(loaderData); // Check if loaderData is correct
               </label>
               <select
                 
-                // defaultValue={category}
+                defaultValue={category}
                 
-                // {...register("category", { required: true })}
+                {...register("category", { required: true })}
                 className="select select-bordered w-full"
               >
                 <option disabled value="default">
@@ -114,10 +111,10 @@ console.log(loaderData); // Check if loaderData is correct
               </label>
               <input
                 type="number"
-                // defaultValue={price}
+                defaultValue={price}
             
                 placeholder="Price"
-                // {...register("price", { required: true })}
+                {...register("price", { required: true })}
                 className="input input-bordered w-full"
               />
             </div>
@@ -128,8 +125,8 @@ console.log(loaderData); // Check if loaderData is correct
               <span className="label-text">Recipe Details</span>
             </label>
             <textarea
-            //   defaultValue={recipe}
-            //   {...register("recipe")}
+              defaultValue={recipe}
+              {...register("recipe")}
               className="textarea textarea-bordered h-24"
               placeholder="Bio"
             ></textarea>
@@ -137,7 +134,7 @@ console.log(loaderData); // Check if loaderData is correct
 
           <div className="form-control w-full my-6">
             <input
-            //   {...register("image", { required: true })}
+              {...register("image", { required: true })}
               type="file"
               className="file-input w-full max-w-xs"
             />
